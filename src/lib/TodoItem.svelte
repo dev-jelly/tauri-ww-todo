b9c1048ac42ec3c3ed0e5b79eed0f7856c27e36f
jelly <chm073@gmail.com>
2022-11-26 21:37:49 +0900

    Add Drag And Drop

--- src/lib/TodoItem.svelte
+++ src/lib/TodoItem.svelte
@@ -2,9 +2,9 @@
   import {todos} from './store/todos';
 
   export let id: number;
-  export let todo: string;
+  export let todoTitle: string;
   export let done: boolean;
-
+  export let index: number;
 
   let checkbox;
 
@@ -30,9 +30,11 @@
     // focus out after 1s
     setTimeout(() => e.target.blur(), 300);
   }
+
 </script>
 
-<li class="w-full rounded-t-lg border-b border-gray-200 dark:border-gray-600 hover:curs or-pointer"
+<div
+    class="w-full"
     on:click|preventDefault={toggleDone}>
   <div class="flex relative items-center pl-3">
     <input id="todo-checkbox-{id}" type="checkbox"
@@ -42,11 +44,11 @@
            on:focus={focus}
            class="w-4 h-4 text-gray-500 bg-gray-100 rounded border-gray-900 focus:ring-gray-400 dark:focus:ring-gray-500 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-900">
     <label for="todo-checkbox-{id}" class="py-3 ml-2 w-full text-md font-medium text-gray-900 dark:text-gray-300 "
-           class:line-through={done} class:text-gray-500={done}>{todo}</label>
+           class:line-through={done} class:text-gray-500={done}>{todoTitle}</label>
     <button
         on:click={onClickDelete}
         class="text-black dark:text-white font-bold absolute right-3 top-2 bottom-2 bg-none hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-2xl px-3 leading-5 pb-1 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-800">
       -
     </button>
   </div>
-</li>
+</div>
--- src/lib/TodoList.svelte
+++ src/lib/TodoList.svelte
@@ -1,11 +1,55 @@
 <script lang="ts">
   import TodoItem from './TodoItem.svelte'
   import {todos} from './store/todos'
+  import {flip} from 'svelte/animate';
+
+  let hovering = false;
+
+  const drop = (event, target) => {
+    event.dataTransfer.dropEffect = 'move';
+    const start = parseInt(event.dataTransfer.getData("text/plain"));
+    const newTodos = todos
+
+    if (start < target) {
+      newTodos.update((todos) => {
+        todos.splice(target + 1, 0, todos[start]);
+        todos.splice(start, 1);
+        return todos;
+      });
+    } else {
+      newTodos.update((todos) => {
+        todos.splice(target, 0, todos[start]);
+        todos.splice(start + 1, 1);
+        return todos;
+      });
+    }
+    hovering = null
+
+  }
+
+  const dragstart = (event, i) => {
+    event.dataTransfer.effectAllowed = 'move';
+    event.dataTransfer.dropEffect = 'move';
+    const start = i;
+    event.dataTransfer.setData('text/plain', start);
+    console.log(start);
+  }
 
 </script>
 
 <ul class="w-full mx-2 md:w-2/3 text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-    {#each $todos as todo (todo.id)}
-        <TodoItem id={todo.id} todo={todo.text} done={todo.done}/>
-    {/each}
+  {#each $todos as todo, index (todo)}
+    <li draggable="true"
+        class="cursor-move rounded-t-lg border-b border-gray-200 dark:border-gray-600 hover:cursor-pointer"
+        animate:flip={{duration: 300}} on:dragstart={event => dragstart(event, index)}
+        on:drop|preventDefault={event => drop(event, index)}
+        on:dragover|preventDefault={() => false}
+        on:dragenter={() => hovering = index}
+        class:opacity-40={hovering === index}
+        class:bg-gray-800={hovering === index}
+    >
+      <TodoItem
+          id={todo.id} index={index} todoTitle={todo.text} done={todo.done}/>
+    </li>
+  {/each}
 </ul>
