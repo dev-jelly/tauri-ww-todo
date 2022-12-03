5b01817557cd62254687ce73f1e6ba6b674a659f
jelly <chm073@gmail.com>
2022-12-03 23:38:02 +0900

    Fix When Drop on Input

--- src/lib/TodoInput.svelte
+++ src/lib/TodoInput.svelte
@@ -27,10 +27,11 @@
 
 <form class="w-2/3" on:submit={onSubmit}>
   <label for="todo" class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
-         on:drop|preventDefault={() => false}>Something To Do...</label>
+         >Something To Do...</label>
   <div class="relative w-full">
     <input type="search" id="todo"
            bind:value={todoTitle}
+           on:drop|preventDefault={() => false}
            class="block w-full p-4 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
            placeholder="Something To Do...">
     <button type="submit"
