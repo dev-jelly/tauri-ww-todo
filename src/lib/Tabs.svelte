e34041e2d169fb6ce9dbea1c9b891d2fa3c21644
jelly <chm073@gmail.com>
2023-01-01 15:44:15 +0900

    Remove Plus Icon When Tab Is Closed

--- src/lib/Tabs.svelte
+++ src/lib/Tabs.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import {tabManager} from "./store/todos";
   import {confirm} from '@tauri-apps/api/dialog';
-  import {cubicOut, elasticInOut, elasticOut} from "svelte/easing";
   import {tweened} from "svelte/motion";
   import {animation} from "./utils/animations";
 
@@ -89,32 +88,34 @@
 </script>
 
 
-<aside bind:this={tabs} class="w-64 h-screen hover:w-64" aria-label="Sidebar" id="tab-side-bar"
+<aside bind:this={tabs} class="w-64 h-screen hover:w-64 group/tabs" aria-label="Sidebar" id="tab-side-bar"
+
        on:mouseover={onMouseOverTabs}
-       on:mouseout={onMouseLeaveTabs}>
+       on:mouseout={onMouseLeaveTabs}
+>
   <div class="overflow-y-auto py-4 px-3 bg-gray-500 rounded-md dark:bg-gray-800 h-full">
     <div class="w-full flex justify-end ">
       <button bind:this={arrow} on:click={toggleOpen} class="px-2">{"<-"}</button>
     </div>
+    <ul class="space-y-2" class:opacity-0={$tabsWidth < 6} class:opacity-100={mouseHover || isOpen}>
+      {#each $tm.tabs as tab, index (tab.id)}
+        <li class="cursor-pointer group relative" on:click={() => setTab(index)}>
+          {#if $tm.tabs.length > 1}
+            <button
+              on:click|stopPropagation={() => removeTab(index)}
+              class="text-black dark:text-white absolute w-6 pr-6 pb-2.5 leading-6 bg-gray-50 group-hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-lg px-4 py-1 dark:bg-gray-800 dark:group-hover:bg-gray-800 dark:hover:group-hover:bg-gray-900 dark:focus:ring-blue-800 right-1 bottom-1 top-1 opacity-0 group-hover:opacity-40 ">
+              -
+            </button>
+          {/if}
+          <a href="#"
+             class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
+            <span class="ml-3">{tab.title}</span>
+          </a>
+        </li>
+      {/each}
+    </ul>
 
-      <ul class="space-y-2" class:opacity-0={$tabsWidth < 6} class:opacity-100={mouseHover || isOpen}>
-        {#each $tm.tabs as tab, index (tab.id)}
-          <li class="cursor-pointer group relative" on:click={() => setTab(index)}>
-            {#if $tm.tabs.length > 1}
-              <button
-                on:click|stopPropagation={() => removeTab(index)}
-                class="text-black dark:text-white absolute w-6 pr-6 pb-2.5 leading-6 bg-gray-50 group-hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-md text-lg px-4 py-1 dark:bg-gray-800 dark:group-hover:bg-gray-800 dark:hover:group-hover:bg-gray-900 dark:focus:ring-blue-800 right-1 bottom-1 top-1 opacity-0 group-hover:opacity-40 ">
-                -
-              </button>
-            {/if}
-            <a href="#"
-               class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white group-hover:bg-gray-100 dark:group-hover:bg-gray-700">
-              <span class="ml-3">{tab.title}</span>
-            </a>
-          </li>
-        {/each}
-      </ul>
-      <form class="flex justify-center w-full h-10 my-2">
+      <form class="flex justify-center w-full h-10 my-2 group-hover/tabs:block" class:hidden={!isOpen}>
         <button type="submit"
                 on:click|preventDefault={addTab}
                 bind:this={addTabButton}
