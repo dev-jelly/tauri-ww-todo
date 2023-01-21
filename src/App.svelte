81f3c7273ef6bd57a18b04f57d27dec7c1bbe21b
jelly <chm073@gmail.com>
2023-01-21 17:53:08 +0900

    Add Zoom In & Out Buttons

--- src/App.svelte
+++ src/App.svelte
@@ -4,15 +4,20 @@
   import Tabs from "./view/tab/Tabs.svelte";
   import TabTitle from "./view/tab/TabTitle.svelte";
   import {tabManager} from "./lib/store/todos";
+  import Zoom from "./view/Zoom.svelte";
   $: tabIndex = $tabManager.tabIndex;
 </script>
 <div class="flex">
   <Tabs/>
   <div class="h-screen container flex m-auto overflow-y-scroll">
-    <main class="h-screen flex flex-col justify-start items-center w-full gap-4 p-4">
+    <main class="min-h-screen flex flex-col justify-start items-center w-full gap-4 p-4 ">
       <TabTitle tabIndex={tabIndex}/>
       <TodoInput/>
       <TodoList/>
+      <div class=" bg-transparent opacity-0 mt-8">
+        _
+      </div>
+      <Zoom />
 
     </main>
   </div>
---
+++ src/view/Zoom.svelte
@@ -0,0 +1,33 @@
+<script  lang="ts">
+  $: defaultFontSize = 16;
+
+  const zoomIn = () => {
+    defaultFontSize += 2;
+  }
+
+  const zoomOut = () => {
+    defaultFontSize -= 2;
+  }
+
+</script>
+
+<div class="fixed bottom-4 right-4 w-24 h-10 border rounded-md overflow-hidden flex justify-center items-center z-10  dark:bg-gray-600 opacity-50 hover:opacity-100 dark:border-gray-400 border-gray-600">
+  <button class="rounded-rl-md dark:hover:bg-gray-400 h-full w-full flex items-center justify-center group cursor-pointer hover:bg-white" on:click={zoomOut}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+         class="w-6 h-6 group-hover:stroke-black">
+      <path stroke-linecap="round" stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM13.5 10.5h-6"/>
+    </svg>
+  </button>
+  <button class="rounded-rl-md dark:hover:bg-gray-400 h-full w-full flex items-center justify-center group cursor-pointer hover:bg-white" on:click={zoomIn}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+         class="w-6 h-6 group-hover:stroke-black">
+      <path stroke-linecap="round" stroke-linejoin="round"
+            d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607zM10.5 7.5v6m3-3h-6"/>
+    </svg>
+  </button>
+</div>
+
+<style global>
+
+</style>
