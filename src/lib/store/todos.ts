04687f5126ba687ead45e623c1d4728a1fb7ac5c
jelly <chm073@gmail.com>
2023-03-12 14:48:46 +0900

    Can Edit Title

--- src/lib/store/todos.ts
+++ src/lib/store/todos.ts
@@ -1,33 +1,42 @@
-import {storable} from "./storable";
-
+import { storable } from "./storable";
 
 export type Todo = {
-  id: number;
+  id: string;
   text: string;
   done: boolean;
-}
+};
 
 export type Tab = {
   id: string;
   title: string;
   todos: Todo[];
-}
+};
 
 export type TabManager = {
   tabIndex: number;
   tabs: Tab[];
+};
 
-}
-
-export const tabManager = storable<TabManager>('tabManager', {
+export const tabManager = storable<TabManager>("tabManager", {
   tabIndex: 0,
-  tabs: [{
-    id: "tab1",
-    title: "탭1",
-    todos: [{ id: 1, text: '샘플 1', done: false }, { id: 2, text: '샘플 2', done: false }, { id: 3, text: '샘플 3', done: false }]
-  }, {
-    id: "tab2",
-    title: "탭 2",
-    todos: [{ id: 1, text: '샘플 1', done: false }, { id: 2, text: '샘플 2', done: false }, { id: 3, text: '샘플 3', done: false }]
-  }]
+  tabs: [
+    {
+      id: "tab1",
+      title: "탭1",
+      todos: [
+        { id: "1", text: "샘플 1", done: false },
+        { id: "2", text: "샘플 2", done: false },
+        { id: "3", text: "샘플 3", done: false },
+      ],
+    },
+    {
+      id: "tab2",
+      title: "탭 2",
+      todos: [
+        { id: "1", text: "샘플 1", done: false },
+        { id: "2", text: "샘플 2", done: false },
+        { id: "3", text: "샘플 3", done: false },
+      ],
+    },
+  ],
 });
--- src/view/todos/TodoItem.svelte
+++ src/view/todos/TodoItem.svelte
@@ -1,14 +1,19 @@
 <script lang="ts">
-  import {tabManager} from '../../lib/store/todos';
+  import { tabManager } from "../../lib/store/todos";
   import { writeText } from "@tauri-apps/api/clipboard";
   import Toast from "../Toast.svelte";
 
-  export let id: number;
+  export let id: string;
   export let todoTitle: string;
   export let done: boolean;
   export let index: number;
-  let open = false;
+  let openToast = false;
   let checkbox;
+
+  let editMode = false;
+
+  let editTitle;
+
 
   const toggleDone = (e) => {
     done = !done;
@@ -18,40 +23,58 @@
       if (index !== -1) {
         tm.tabs[tm.tabIndex].todos[index].done = done;
       }
-      return {...tm};
+      return { ...tm };
     });
-  }
+  };
 
   const onClickDelete = () => {
     tabManager.update((tm) => {
-      const todos =  tm.tabs[tm.tabIndex].todos;
+      const todos = tm.tabs[tm.tabIndex].todos;
       tm.tabs[tm.tabIndex].todos = todos.filter((t) => t.id !== id);
       return tm;
     });
-  }
+  };
 
   const focus = (e) => {
     // focus out after 1s
     setTimeout(() => e.target.blur(), 300);
-  }
+  };
 
   const onCopy = async (e) => {
     e.preventDefault();
-    await writeText(todoTitle)
-    open = true;
-    setTimeout(() => open = false, 2000);
-  }
+    await writeText(todoTitle);
+    openToast = true;
+    setTimeout(() => openToast = false, 2000);
+  };
 
+
+  const onUpdateTitle = (id: string) => {
+    tabManager.update((tm) => {
+      const tabIndex = tm.tabIndex;
+      const todoIndex = tm.tabs[tabIndex].todos.findIndex(t => t.id == id);
+      tm.tabs[tabIndex].todos[todoIndex].text = editTitle.value;
+      console.log(editTitle.value, tabIndex, todoIndex)
+      tm.tabs[tabIndex].todos = [...tm.tabs[tabIndex].todos];
+      tm.tabs = [...tm.tabs];
+      console.log(tm);
+      return { ...tm };
+    });
+  };
+
+  const onClickUpdate = () => {
+    editMode = true;
+    editTitle.focus();
+  };
 
 
 </script>
 
 <div
-    class="w-full  cursor-pointer"
-    on:click|preventDefault={toggleDone}
-    on:contextmenu|stopPropagation={onCopy}
+  class="w-full cursor-pointer"
+  on:click|preventDefault={toggleDone}
+  on:contextmenu|stopPropagation={onCopy}
 >
-  <Toast open={open} title="복사가 되었습니다!" />
+  <Toast open={openToast} title="복사가 되었습니다!" />
   <div class="flex relative items-center pl-3 cursor-pointer">
     <input id="todo-checkbox-{id}" type="checkbox"
            bind:this={checkbox}
@@ -59,13 +82,28 @@
            on:click|stopPropagation={toggleDone}
            on:focus={focus}
            on:contextmenu|stopPropagation={onCopy}
-
            class="w-4 h-4 text-gray-500 bg-gray-100 rounded border-gray-900 focus:ring-gray-400 dark:focus:ring-gray-500 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-900 ">
-    <label for="todo-checkbox-{id}" class="py-3 ml-2 w-full text-md font-medium text-gray-900 dark:text-gray-300 overflow-ellipsis overflow-hidden pr-6  cursor-pointer"
-           class:line-through={done} class:text-gray-500={done}>{todoTitle}</label>
+
+      <input bind:this={editTitle} type="text" on:click|stopPropagation bind:value={todoTitle}
+             class:hidden={!editMode}
+             on:blur={() => editMode = false}
+             on:keypress|stopPropagation={(e) => {
+        if (e.key === "Enter") {
+          onUpdateTitle(id);
+          editMode = false;
+
+        }
+      }}
+             class="ml-2 h-11 text-gray-200 bg-gray-100 rounded border-gray-900  focus:ring-2  p-2 dark:bg-gray-600 dark:border-gray-900 w-full" />
+
+      <label on:dblclick|stopPropagation={onClickUpdate} for="todo-checkbox-{id}"
+             class:hidden={editMode}
+             class="py-3 ml-2 w-full text-md font-medium text-gray-900 dark:text-gray-300 overflow-ellipsis overflow-hidden pr-6 cursor-pointer"
+             class:line-through={done} class:text-gray-500={done}>{todoTitle}</label>
+
     <button
-        on:click={onClickDelete}
-        class="text-black dark:text-white font-bold absolute right-3 top-2 bottom-2 bg-none hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-2xl px-3 leading-5 pb-1 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-800">
+      on:click={onClickDelete}
+      class="text-black dark:text-white font-bold absolute right-3 top-2 bottom-2 bg-none hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-md text-2xl px-3 leading-5 pb-1 dark:bg-gray-700 dark:hover:bg-gray-800 dark:focus:ring-gray-800">
       -
     </button>
   </div>
